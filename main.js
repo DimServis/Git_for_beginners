7a27a83e50dfcf2d70f1608c9a09d313bb1c0917
dimakar79 <89477881+dimakar79@users.noreply.github.com>
2022-05-02 22:14:25 +0500

    Создали скрипт файл
    начали обучение

---
+++ main.js
@@ -0,0 +1,8 @@
+"use strict";
+let message = 'Hello world';
+message = 'Hello Dim';
+alert(message);
+let mes = prompt('Введите имя');
+alert(`Hello ${mes}`);
+let bos = confirm('Введите');
+alert(bos);
